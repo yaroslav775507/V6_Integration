32e70c1a899e528f8499742a19cbb8815377c662
yaroslav <redyar55@gmail.com>
2024-09-01 16:37:01 +0300

    add

--- src/main/java/yaroslav/TextSorter.java
+++ src/main/java/yaroslav/TextSorter.java
@@ -17,6 +17,49 @@ public class TextSorter {
 
         String inputFile = defaultInputFile;
         String outputFile = defaultOutputFile;
+        Path filePath = Paths.get(defaultInputFile);
+
+        String content = "Кошка убегает от собаки\n"
+                + "Собака пытается догнать кошку\n"
+                + "Мышка спокойно сидит в своей норе\n"
+                + "Собака пытается догнать кошку";
+
+        try {
+            Path inputFilePath = Paths.get(defaultInputFile);
+            Path outputFilePath = Paths.get(defaultOutputFile);
+
+            // Проверка и создание входного файла
+            if (!Files.exists(inputFilePath)) {
+                System.out.println("Входной файл не был найден.");
+                // Создание директории, если она не существует
+                Files.createDirectories(inputFilePath.getParent());
+                // Создание файла
+                Files.createFile(inputFilePath);
+                System.out.println("Входной файл создан: " + inputFilePath);
+            } else {
+                System.out.println("Входной файл существует: " + inputFilePath);
+            }
+
+            // Запись текста в входной файл, если он был создан или уже существует
+            Files.write(inputFilePath, content.getBytes());
+            System.out.println("Текст успешно записан в файл: " + inputFilePath);
+
+            // Проверка и создание выходного файла
+            if (!Files.exists(outputFilePath)) {
+                System.out.println("Выходной файл не был найден.");
+                // Создание директории, если она не существует
+                Files.createDirectories(outputFilePath.getParent());
+                // Создание файла
+                Files.createFile(outputFilePath);
+                System.out.println("Выходной файл создан: " + outputFilePath);
+            } else {
+                System.out.println("Выходной файл существует: " + outputFilePath);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Ошибка при создании файлов или записи: " + e.getMessage());
+        }
+
 
 
         System.out.println("Выберите тип сортировки:");
